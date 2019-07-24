f736a0cdbe21fda712c9e910c96488a28930a0a9
aldidev <aldiendz@gmail.com>
2019-07-24 17:25:40 +0700

    blud kas add service simpan

--- src/js/controllers/kas.js
+++ src/js/controllers/kas.js
@@ -1,36 +1,81 @@
 angular
-    .module('app', ['toaster', 'ngAnimate', 'datatables','angularMoment'])
-    .controller('KasController',KasController);
+    .module('app', ['toaster', 'ngAnimate', 'datatables'])
+    .controller('KasController', function ($scope, toaster,kasService) {
+        
+        const vm = this;
+        $scope.readonlySaldo = false;
 
-    function KasController(DTOptionsBuilder, DTColumnBuilder,$scope,moment) {
-        $scope.tahun = new Date();
-        $scope.exampleDate =moment($scope.tahun).format('YYYY');
-        console.log($scope.exampleDate)
-        var vm = this;
-        var local = JSON.parse(localStorage.getItem('currentUser'));
-        var authorization = 'Bearer ' + local.access_token;
-        console.log(authorization)
-        vm.dtOptions = DTOptionsBuilder.newOptions()
-            .withOption('ajax', {
-             // Either you specify the AjaxDataProp here
-             // dataSrc: 'data',
-             headers: { 'Authorization': authorization,'Content-Type': 'application/json; charset=UTF-8' },
-             url: '/blud-auth-server/api/oauth/token/current/datatables',
-             type: 'POST',
-             data: function(data){
-                return JSON.stringify(data);
-             }
+        //formBuild
+        $scope.formData;
+        $scope.valData;
+        
+        kasService.findDataApi(function (result) {
+            if (result.status === 200) {
+                console.log('Response Result Datatable Data');
+                console.log(result.data);
+                $scope.valData = result.data;
+                console.log()
+            } else {
+                console.log('Response Result Datatable Data');
+                console.log(result);
+                // $scope.vm.error = 'Username or password is incorrect';
+                // $scope.vm.loading = false;
+            }
+        });
 
-         })
-         // or here
-         .withDataProp('data')
-            .withOption('processing', true)
-            .withOption('searching', false)
-            .withOption('serverSide', true)
-            .withPaginationType('full_numbers');
-        vm.dtColumns = [
-            DTColumnBuilder.newColumn('username').withTitle('Kode Akun'),
-            DTColumnBuilder.newColumn('clientId').withTitle('Perkiraan Saldo Kas Per 31 Desember ' + $scope.exampleDate),
-            DTColumnBuilder.newColumn('clientId').withTitle('Perkiraan Saldo Kas Per 31 Desember ')
-        ];
-    }
+        // $scope.saveData = function() {
+        //     console.log('you can save all the valData as a document: ');
+        //     console.log($scope.valData);
+        //     console.log('or save row by row:');
+        //     var index = 0;
+        //     $scope.valData.forEach(function (row) {
+        //         console.log('row #' + (index++) + ': ' + JSON.stringify(row));
+        //     });
+        // }
+
+        
+    $scope.saveData = function () {
+        $scope.valData.forEach(function (row) {
+                    console.log('row #'+ ': ' + JSON.stringify(row));
+                });
+        kasService.saveData(null,$scope.valData, function (result) {
+            console.log(result.data);
+            if (result.status === 200) {
+                console.log('Response Result Delete Data');
+                console.log(result);
+                toaster.pop({
+                    type: 'success',
+                    title: 'Berhasil Hapus Data',
+                    body: 'Data Jenis Pelanggaran Berhasil Dihapus',
+                    timeout: 5000
+                });
+                $location.path('/components/list-example');
+            } else {
+                console.log('Response Result Delete Data');
+                console.log(result);
+            }
+        });
+    };
+
+     
+
+        $scope.suntingAnggaran = function(){
+          $scope.readonlySaldo = true;
+          
+         }
+
+        $scope.pop = function () {
+            toaster.pop('info', "title", "text");
+        };
+
+        $scope.testToastr = function () {
+            console.log('Testing Toastr');
+            // toaster.pop('info', "title", "text");
+            toaster.pop({
+                type: 'error',
+                title: 'Title text',
+                body: 'Body text',
+                timeout: 3000
+            });
+        }
+    });
---
+++ src/js/services/kas.service.js
@@ -0,0 +1,45 @@
+angular
+    .module('app')
+    .factory('kasService', ['$http', '$q', function ($http, $q) {
+        return {
+            //Code edited to create a function as when you require service it returns object by default so you can't return function directly. That's what understand...
+            findDataApi: function (callback) {
+                const req = {
+                    method: 'GET',
+                    url: 'https://api.myjson.com/bins/1ftdvt'
+                };
+                $http(req)
+                    .then(function (response) {
+                        console.log('Service Response Find Data');
+                        console.log(response);
+                        // login successful if there's a token in the response
+                        callback(response);
+                    })
+            },
+            saveData: function (params, body, callback) {
+                console.log(params)
+                console.log(body)
+                console.log(callback)
+                // const local = JSON.parse(localStorage.getItem('currentUser'));
+                // const token = 'Bearer ' + local.access_token;
+                // const req = {
+                //     method: 'POST',
+                //     url: url,
+                //     headers: {
+                //         'Authorization': token,
+                //         "Accept": "application/json",
+                //         "Content-Type": "application/json;charset=utf-8"
+                //     },
+                //     params: params,
+                //     data: body
+                // };
+                // $http(req)
+                //     .then(function (response) {
+                //         // login successful if there's a token in the response
+                //         console.log(`service response api get ${url} :`);
+                //         console.log(response);
+                //         callback(response);
+                //     });
+            },
+        }
+    }]);
